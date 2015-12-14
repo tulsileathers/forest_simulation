2555aaa54cd20bfa15f4bcef2c06ed29904d4373
Jayce Rudig-Leathers <me@jayceleathers.com>
2015-12-14 11:50:52 -0500

    adds superclass for tree types

---
+++ Tree.m
@@ -0,0 +1,17 @@
+classdef Tree
+    properties
+        %Tree species specific variables
+        G	%Parameter that determines how early in its age (or size) a tree achieves most of its growth
+        Dmax	%Maximum diameter at breast height (cm)
+        Hmax	%Maximum height (cm)
+        AGEmax	%Maximum age (years)
+        C	%Constant of proportionality relating leaf area to tree diameter
+        ALmin	%Minimum proportion of incident sunlight needed for regeneration
+        ALmax	%Maximum proportion of incident sunlight needed for regeneration
+        b2	= 2 * (Hmax - 137)/Dmax;
+        b3	= (Hmax - 137)/(Dmax^2);
+    end
+    methods
+        
+    end
+end
